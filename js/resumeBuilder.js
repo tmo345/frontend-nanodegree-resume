a33d2bde6fe241f823f37453159c710334afb940
tmo345 <moore.tim@outlook.com>
2015-11-04 10:27:48 -0500

    Add education object to resumebuilder, delete hello world from index.html

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,53 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var education = {
+    "schools": [
+        {
+            "school": "University of Georgia",
+            "city": "Athens, Ga",
+            "majors": ["Microbiology"],
+            "graduationYear": 2005
+        },
+        {
+            "school": "Medical College of Georgia",
+            "city": "Augusta, Ga",
+            "majors": ["Medicine (MD)"],
+            "graduationYear": 2009
+        },
+        "onlineCourses": [
+            {
+                "title": "Intro to HTML and CSS",
+                "school": "Udacity",
+                "dates": "September - October 2015",
+                "url": "https://www.udacity.com/course/viewer#!/c-ud304-nd"
+            },
+            {
+                "title": "Responsive Web Design Fundamentals",
+                "school": "Udacity",
+                "dates": "October 2015",
+                "url": "https://www.udacity.com/course/viewer#!/c-ud893-nd"
+            },
+            {
+                "title": "Responsive Images",
+                "school": "Udacity",
+                "dates": "October 2015 - Present",
+                "url": "https://www.udacity.com/course/viewer#!/c-ud882-nd"
+            },
+            {
+                "title": "How to Use Git and GitHub",
+                "school": "Udacity",
+                "dates": "October 2015 - Present",
+                "url": "https://www.udacity.com/course/viewer#!/c-ud775-nd/"
+            }
+            {
+                "title": "Javascript Basics",
+                "school": "Udacity",
+                "dates": "November 2015 - Present",
+                "url": "https://www.udacity.com/course/viewer#!/c-ud804-nd"
+            }
+        ]
+    ]
+}
+
+
+
+
+

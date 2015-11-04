6d399a24178b458c51627887c4b5a9c924abbc42
tmo345 <moore.tim@outlook.com>
2015-11-04 10:39:28 -0500

    Add work object to resumebuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,44 @@
+var work = {
+    "jobs": [
+        {
+            "employer": "Wake Forest University Baptist Medical Center",
+            "title": "Pediatrics Resident Physician",
+            "location": "Winston-Salem, NC",
+            "dates": "July 2009 - June 2011",
+            "description": "Physician in pediatrics residency training program. Duties included care of patients in both inpatient and outpatient settings."
+        },
+        {
+            "employer": "Grovetown High School",
+            "title": "Science Teacher",
+            "location": "Grovetown, GA",
+            "dates": "August 2012 - April 2013",
+            "description": "Taught Environmental Science and Biology to high school freshman and sophpmores"
+        },
+        {
+            "employer": "Medical College of Georgia at Georgia Regents University",
+            "title": "Consultant",
+            "location": "Augusta, GA",
+            "dates": "December 2013 - June 2015",
+            "description": "Wrote and developed online course materials for third year medical student clerkships."
+        },
+        {
+            "employer": "Self-employed",
+            "title": "Freelance WordPress Developer",
+            "location": "Augusta, GA",
+            "dates": "February 2015 - October 2015",
+            "description": "Building and maintaining WordPress websites for businesses and organizations"
+        },
+        {
+            "employer": "Silver Cittern Web Development, LLC",
+            "title": "Owner and WordPress Developer",
+            "location": "Augusta, Ga",
+            "dates": "October 2015 - Present",
+            "description": "Own and operate business and build/maintain WordPress websites for businesses and organizations."
+        }
+    ]
+}
+
+
 var education = {
     "schools": [
         {

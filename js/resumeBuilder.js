993c59ab578f1aa08949ef4f125ba9c053bd529a
tmo345 <moore.tim@outlook.com>
2015-11-04 12:26:05 -0500

    Fix JSON Lint validation error in education object, move online course array to be second property instead of part of schools array

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -98,39 +98,39 @@ var education = {
             "city": "Augusta, Ga",
             "majors": ["Medicine (MD)"],
             "graduationYear": 2009
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Intro to HTML and CSS",
+            "school": "Udacity",
+            "dates": "September - October 2015",
+            "url": "https://www.udacity.com/course/viewer#!/c-ud304-nd"
         },
-        "onlineCourses": [
-            {
-                "title": "Intro to HTML and CSS",
-                "school": "Udacity",
-                "dates": "September - October 2015",
-                "url": "https://www.udacity.com/course/viewer#!/c-ud304-nd"
-            },
-            {
-                "title": "Responsive Web Design Fundamentals",
-                "school": "Udacity",
-                "dates": "October 2015",
-                "url": "https://www.udacity.com/course/viewer#!/c-ud893-nd"
-            },
-            {
-                "title": "Responsive Images",
-                "school": "Udacity",
-                "dates": "October 2015 - Present",
-                "url": "https://www.udacity.com/course/viewer#!/c-ud882-nd"
-            },
-            {
-                "title": "How to Use Git and GitHub",
-                "school": "Udacity",
-                "dates": "October 2015 - Present",
-                "url": "https://www.udacity.com/course/viewer#!/c-ud775-nd/"
-            }
-            {
-                "title": "Javascript Basics",
-                "school": "Udacity",
-                "dates": "November 2015 - Present",
-                "url": "https://www.udacity.com/course/viewer#!/c-ud804-nd"
-            }
-        ]
+        {
+            "title": "Responsive Web Design Fundamentals",
+            "school": "Udacity",
+            "dates": "October 2015",
+            "url": "https://www.udacity.com/course/viewer#!/c-ud893-nd"
+        },
+        {
+            "title": "Responsive Images",
+            "school": "Udacity",
+            "dates": "October 2015 - Present",
+            "url": "https://www.udacity.com/course/viewer#!/c-ud882-nd"
+        },
+        {
+            "title": "How to Use Git and GitHub",
+            "school": "Udacity",
+            "dates": "October 2015 - Present",
+            "url": "https://www.udacity.com/course/viewer#!/c-ud775-nd/"
+        },
+        {
+            "title": "Javascript Basics",
+            "school": "Udacity",
+            "dates": "November 2015 - Present",
+            "url": "https://www.udacity.com/course/viewer#!/c-ud804-nd"
+        }
     ]
 }
 

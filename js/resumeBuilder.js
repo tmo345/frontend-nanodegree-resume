9b46040a74d11b46ff85db29884667bdeb9dfe69
tmo345 <moore.tim@outlook.com>
2015-11-05 15:28:40 -0500

    Add display method to education object and call to show school

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,172 +1,201 @@
 var bio = {
-    "name": "Timothy Moore",
-    "role": "Web Developer",
-    "contacts": {
-        "email": "moore.tim@outlook.com",
-        "github": "tmo345",
-        "location": "Augusta, GA"
+    'name': 'Timothy Moore',
+    'role': 'Web Developer',
+    'contacts': {
+        'email': 'moore.tim@outlook.com',
+        'github': 'tmo345',
+        'location': 'Augusta, GA'
     },
-    "welcomeMessage": "Welcome to my resume.",
-    "skills": ["html", "css", "javascript", "jquery", "php", "WordPress", "Genesis Framework"],
-    "image": "images/Timothy_Moore.jpg",
-    "biopic": "www.timothymoore.me",
+    'welcomeMessage': 'Welcome to my resume.',
+    'skills': ['html', 'css', 'javascript', 'jquery', 'php', 'WordPress', 'Genesis Framework'],
+    'image': 'images/Timothy_Moore.jpg',
     // display method
-    "display": function() {
-        $('#header').prepend(HTMLheaderRole.replace('%data%', bio.role));
-        $('#header').prepend(HTMLheaderName.replace('%data%', bio.name));
+    'display': function() {
+        $('#header').prepend(HTMLheaderRole.replace('%data%', this.role));
+        $('#header').prepend(HTMLheaderName.replace('%data%', this.name));
 
-        for (var contact in bio.contacts) {
-            if (bio.contacts.hasOwnProperty(contact)) {
-                $('#topContacts').append(HTMLcontactGeneric.replace('%contact%', contact).replace('%data%', bio.contacts[contact]));
+        for (var contact in this.contacts) {
+            if (this.contacts.hasOwnProperty(contact)) {
+                $('#topContacts').append(HTMLcontactGeneric.replace('%contact%', contact).replace('%data%', this.contacts[contact]));
             }
         }
 
-        $('#header').append(HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage));
-        $('#header').append(HTMLbioPic.replace('%data%', bio.image));
+        $('#header').append(HTMLwelcomeMsg.replace('%data%', this.welcomeMessage));
+        $('#header').append(HTMLbioPic.replace('%data%', this.image));
 
-        if (bio.skills.length > 0) {
+        if (this.skills.length > 0) {
             $('#header').append(HTMLskillsStart);
-            for (var x = 0; x < bio.skills.length; x++) {
-                $('#skills').append(HTMLskills.replace('%data%', bio.skills[x]));
+            for (var x = 0; x < this.skills.length; x++) {
+                $('#skills').append(HTMLskills.replace('%data%', this.skills[x]));
             }
         }
     }
 }
 
 var work = {
-    "jobs": [
+    'jobs': [
         {
-            "employer": "Wake Forest University Baptist Medical Center",
-            "title": "Pediatrics Resident Physician",
-            "location": "Winston-Salem, NC",
-            "dates": "July 2009 - June 2011",
-            "description": "Physician in pediatrics residency training program. Duties included care of patients in both inpatient and outpatient settings."
+            'employer': 'Wake Forest University Baptist Medical Center',
+            'title': 'Pediatrics Resident Physician',
+            'location': 'Winston-Salem, NC',
+            'dates': 'July 2009 - June 2011',
+            'description': 'Physician in pediatrics residency training program. Duties included care of patients in both inpatient and outpatient settings.'
         },
         {
-            "employer": "Grovetown High School",
-            "title": "Science Teacher",
-            "location": "Grovetown, GA",
-            "dates": "August 2012 - April 2013",
-            "description": "Taught Environmental Science and Biology to high school freshman and sophomores."
+            'employer': 'Grovetown High School',
+            'title': 'Science Teacher',
+            'location': 'Grovetown, GA',
+            'dates': 'August 2012 - April 2013',
+            'description': 'Taught Environmental Science and Biology to high school freshman and sophomores.'
         },
         {
-            "employer": "Medical College of Georgia at Georgia Regents University",
-            "title": "Consultant",
-            "location": "Augusta, GA",
-            "dates": "December 2013 - June 2015",
-            "description": "Wrote and developed online course materials for third year medical student clerkships."
+            'employer': 'Medical College of Georgia at Georgia Regents University',
+            'title': 'Consultant',
+            'location': 'Augusta, GA',
+            'dates': 'December 2013 - June 2015',
+            'description': 'Wrote and developed online course materials for third year medical student clerkships.'
         },
         {
-            "employer": "Self-employed",
-            "title": "Freelance Web Developer",
-            "location": "Augusta, GA",
-            "dates": "February 2015 - October 2015",
-            "description": "Building and maintaining WordPress websites for businesses and organizations"
+            'employer': 'Self-employed',
+            'title': 'Freelance Web Developer',
+            'location': 'Augusta, GA',
+            'dates': 'February 2015 - October 2015',
+            'description': 'Building and maintaining WordPress websites for businesses and organizations'
         },
         {
-            "employer": "Silver Cittern Web Development, LLC",
-            "title": "Owner and Web Developer",
-            "location": "Augusta, Ga",
-            "dates": "October 2015 - Present",
-            "description": "Own and operate business and build/maintain WordPress websites for businesses and organizations."
+            'employer': 'Silver Cittern Web Development, LLC',
+            'title': 'Owner and Web Developer',
+            'location': 'Augusta, Ga',
+            'dates': 'October 2015 - Present',
+            'description': 'Own and operate business and build/maintain WordPress websites for businesses and organizations.'
         }
     ],
-    "display": function() {
-        work.jobs.forEach(function(job){
-        $('#workExperience').append(HTMLworkStart);
-        $('.work-entry:last').append(HTMLworkEmployer.replace('%data%', job.employer) + HTMLworkTitle.replace('%data%', job.title));
-        $('.work-entry:last').append(HTMLworkLocation.replace('%data%', job.location));
-        $('.work-entry:last').append(HTMLworkDates.replace('%data%', job.dates));
-        $('.work-entry:last').append(HTMLworkDescription.replace('%data%', job.description));
+    'display': function() {
+        this.jobs.forEach(function(job){
+            $('#workExperience').append(HTMLworkStart);
+            $('.work-entry:last').append(HTMLworkEmployer.replace('%data%', job.employer) + HTMLworkTitle.replace('%data%', job.title));
+            $('.work-entry:last').append(HTMLworkLocation.replace('%data%', job.location));
+            $('.work-entry:last').append(HTMLworkDates.replace('%data%', job.dates));
+            $('.work-entry:last').append(HTMLworkDescription.replace('%data%', job.description));
         });
     }
 }
 
-var projects = {
-    "projects": [
-        {
-            "title": "Mahmudah Institute of Wellness and Mindful Living Website",
-            "dates": "Live March 2015, Maintained March 2015 - Present",
-            "description": "Website for nonprofit organization built using Genesis Framework on WordPress. Customized Outreach Pro Genesis child theme. www.mahmudahinstitute.org",
-            "images": ["images/Mahmudah_Institute_Featured-1-740x735.jpg"]
-        },
-        {
-            "title": "Augusta Retina Consultants Website",
-            "dates": "Live May 2015, Maintained May 2015 - Present",
-            "description": "Website for ophthalmologist's practice built using Genesis Framework on WordPress. Customized Executive Pro Genesis child theme. www.augustaretina.com",
-            "images": ["images/Augusta_Retina-740x737.jpg"]
-        },
-        {
-            "title": "Timothy J. Moore Portfolio Website",
-            "dates": "Live October 2015, Maintained October 2015 - Present",
-            "description": "Portfolio website and contact for Silver Cittern Web Development built using Genesis Framework on WordPress. Customized Modern Portfolio Pro Genesis child theme. www.timothymoore.me",
-            "images": ["images/timothymoore-500.jpg"]
-        },
-        {
-            "title": "Horizon Eye Center Website",
-            "dates": "Live November 2015",
-            "description": "Website for ophthalmologist practice built using Genesis Framework on WordPress. Customized Altitude Pro Genesis child theme. wwww.horizoneyecenter.com",
-            "images": ["images/HEC.jpg"]
-        },
-        {
-            "title": "Portfolio - Udacity Front-End Web Developer Nanodegree",
-            "dates": "October 2015",
-            "description": "Built and customized style of portfolio site from mockup",
-            "images": ["images/Portfolio-P1.jpg"]
-        }
-    ]
-}
-
-
-
 var education = {
-    "schools": [
+    'schools': [
         {
-            "school": "University of Georgia",
-            "location": "Athens, Ga",
-            "majors": ["Microbiology"],
-            "graduationYear": 2005
+            'name': 'University of Georgia',
+            'location': 'Athens, Ga',
+            'degree': 'B.S. Microbiology',
+            'majors': ['Microbiology'],
+            'dates': 2005,
+            'url': 'http://www.uga.edu'
         },
         {
-            "school": "Medical College of Georgia",
-            "location": "Augusta, Ga",
-            "majors": ["Medicine (MD)"],
-            "graduationYear": 2009
+            'name': 'Medical College of Georgia',
+            'location': 'Augusta, Ga',
+            'degree': 'M.D.',
+            'majors': ['Medicine'],
+            'dates': 2009,
+            'url': 'http://www.gru.edu/mcg/',
         }
     ],
-    "onlineCourses": [
+    'onlineCourses': [
         {
-            "title": "Intro to HTML and CSS",
-            "school": "Udacity",
-            "dates": "September - October 2015",
-            "url": "https://www.udacity.com/course/viewer#!/c-ud304-nd"
+            'title': 'Intro to HTML and CSS',
+            'school': 'Udacity',
+            'dates': 'October 2015',
+            'url': 'https://www.udacity.com/course/viewer#!/c-ud304-nd'
         },
         {
-            "title": "Responsive Web Design Fundamentals",
-            "school": "Udacity",
-            "dates": "October 2015",
-            "url": "https://www.udacity.com/course/viewer#!/c-ud893-nd"
+            'title': 'Responsive Web Design Fundamentals',
+            'school': 'Udacity',
+            'dates': 'October 2015',
+            'url': 'https://www.udacity.com/course/viewer#!/c-ud893-nd'
         },
+        // {
+        //     'title': 'Responsive Images',
+        //     'school': 'Udacity',
+        //     'dates': 'October 2015 - Present',
+        //     'url': 'https://www.udacity.com/course/viewer#!/c-ud882-nd'
+        // },
+        // {
+        //     'title': 'How to Use Git and GitHub',
+        //     'school': 'Udacity',
+        //     'dates': 'October 2015 - Present',
+        //     'url': 'https://www.udacity.com/course/viewer#!/c-ud775-nd/'
+        // },
         {
-            "title": "Responsive Images",
-            "school": "Udacity",
-            "dates": "October 2015 - Present",
-            "url": "https://www.udacity.com/course/viewer#!/c-ud882-nd"
-        },
-        {
-            "title": "How to Use Git and GitHub",
-            "school": "Udacity",
-            "dates": "October 2015 - Present",
-            "url": "https://www.udacity.com/course/viewer#!/c-ud775-nd/"
-        },
-        {
-            "title": "Javascript Basics",
-            "school": "Udacity",
-            "dates": "November 2015 - Present",
-            "url": "https://www.udacity.com/course/viewer#!/c-ud804-nd"
+            'title': 'Javascript Basics',
+            'school': 'Udacity',
+            'dates': 'November 2015',
+            'url': 'https://www.udacity.com/course/viewer#!/c-ud804-nd'
         }
-    ]
+    ],
+    'display': function() {
+        this.schools.forEach(function(school){
+            $('#education').append(HTMLschoolStart);
+
+            var schoolName = HTMLschoolName.replace('%data%', school.name).replace('#', school.url);
+            var schoolDegree = HTMLschoolDegree.replace('%data%', school.degree);
+            $('.education-entry:last').append(schoolName + schoolDegree);
+            $('.education-entry:last').append(HTMLschoolLocation.replace('%data%', school.location));
+            $('.education-entry:last').append(HTMLschoolDates.replace('%data%', school.dates));
+            for (var x = 0; x < school.majors.length; x++) {
+                $('.education-entry:last').append(HTMLschoolMajor.replace('%data%', school.majors[x]));
+            }
+        });
+    }
+
 }
+
+var projects = {
+    'projects': [
+        {
+            'title': 'Mahmudah Institute of Wellness and Mindful Living Website',
+            'dates': 'Live March 2015, Maintained March 2015 - Present',
+            'description': 'Website for nonprofit organization built using Genesis Framework on WordPress. Customized Outreach Pro Genesis child theme. www.mahmudahinstitute.org',
+            'images': ['images/Mahmudah_Institute_Featured-1-740x735.jpg']
+        },
+        {
+            'title': 'Augusta Retina Consultants Website',
+            'dates': 'Live May 2015, Maintained May 2015 - Present',
+            'description': 'Website for ophthalmologist\'s practice built using Genesis Framework on WordPress. Customized Executive Pro Genesis child theme. www.augustaretina.com',
+            'images': ['images/Augusta_Retina-740x737.jpg']
+        },
+        {
+            'title': 'Timothy J. Moore Portfolio Website',
+            'dates': 'Live October 2015, Maintained October 2015 - Present',
+            'description': 'Portfolio website and contact for Silver Cittern Web Development built using Genesis Framework on WordPress. Customized Modern Portfolio Pro Genesis child theme. www.timothymoore.me',
+            'images': ['images/timothymoore-500.jpg']
+        },
+        {
+            'title': 'Horizon Eye Center Website',
+            'dates': 'Live November 2015',
+            'description': 'Website for ophthalmologist practice built using Genesis Framework on WordPress. Customized Altitude Pro Genesis child theme. wwww.horizoneyecenter.com',
+            'images': ['images/HEC.jpg']
+        },
+        {
+            'title': 'Portfolio - Udacity Front-End Web Developer Nanodegree',
+            'dates': 'October 2015',
+            'description': 'Built and customized style of portfolio site from mockup',
+            'images': ['images/Portfolio-P1.jpg']
+        }
+    ],
+    'display': function() {
+        this.projects.forEach(function(project) {
+            $('#projects').append(HTMLprojectStart);
+            $('.project-entry:last').append(HTMLprojectTitle.replace('%data%', project.title));
+            $('.project-entry:last').append(HTMLprojectDates.replace('%data%', project.dates));
+            $('.project-entry:last').append(HTMLprojectDescription.replace('%data%', project.description));
+            $('.project-entry:last').append(HTMLprojectImage.replace('%data%', project.images));
+        });
+    }
+}
+
+
+
+
 
 var inName = function() {
     var nameArray = bio.name.trim().split(' ');
@@ -183,19 +212,11 @@ $(document).click(function(loc) {
 
 $('#main').append(internationalizeButton);
 
-projects.display = function() {
-    projects.projects.forEach(function(project) {
-        $('#projects').append(HTMLprojectStart);
-        $('.project-entry:last').append(HTMLprojectTitle.replace('%data%', project.title));
-        $('.project-entry:last').append(HTMLprojectDates.replace('%data%', project.dates));
-        $('.project-entry:last').append(HTMLprojectDescription.replace('%data%', project.description));
-        $('.project-entry:last').append(HTMLprojectImage.replace('%data%', project.images));
-    });
-};
 
 bio.display();
 work.display();
 projects.display();
+education.display();
 
 // Skills population
 

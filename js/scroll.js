232332cce0478e60503720aadb75dba794549ac9
tmo345 <moore.tim@outlook.com>
2015-11-20 19:11:28 -0500

    Increase scrollspy and scroll target offset

--- js/scroll.js
+++ js/scroll.js
@@ -3,8 +3,8 @@ $(function() {
     $('.menu-item a').each(function() {
         $(this).on('click', function(e) {
             e.preventDefault();
-            // Offset - 75: 60px width of fixed menu + 15px for clearance above target
-            $('body').animate({scrollTop: ($(this.hash).offset().top - 75) }, 1200);
+            // Offset - 85: 60px width of fixed menu + 25px for clearance above target
+            $('body').animate({scrollTop: ($(this.hash).offset().top - 85) }, 1200);
         });
     });
 });
